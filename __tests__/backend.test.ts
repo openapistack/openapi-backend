4ebdc383ea828cddc3761d8330de8d03e4850627
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2018-10-27 11:11:06 +0300

    Implement operation matching

--- __tests__/backend.test.ts
+++ __tests__/backend.test.ts
@@ -1,12 +1,63 @@
-import fs from 'fs';
-import path from 'path';
 import OpenAPIBackend from '../src/index';
+import { OpenAPIV3 } from 'openapi-types';
 
-const documentPath = path.join(__dirname, 'resources', 'example-pet-api.openapi.json');
-const document = JSON.parse(fs.readFileSync(documentPath).toString());
+const responses = {
+  200: { description: 'ok' },
+};
+
+const document: OpenAPIV3.Document = {
+  openapi: '3.0.0',
+  info: {
+    title: 'api',
+    version: '1.0.0',
+  },
+  paths: {
+    '/pets': {
+      get: {
+        operationId: 'getPets',
+        responses,
+      },
+      post: {
+        operationId: 'createPet',
+        responses,
+      },
+    },
+    '/pets/{id}': {
+      get: {
+        operationId: 'getPetById',
+        responses,
+      },
+      put: {
+        operationId: 'replacePetById',
+        responses,
+      },
+      patch: {
+        operationId: 'updatePetById',
+        responses,
+      },
+      delete: {
+        operationId: 'deletePetById',
+        responses,
+      },
+    },
+    '/pets/{id}/owner': {
+      get: {
+        operationId: 'getOwnerByPetId',
+        responses,
+      },
+    },
+    '/pets/meta': {
+      get: {
+        operationId: 'getPetsMeta',
+        responses,
+      },
+    },
+  },
+};
 
 describe('OpenAPIBackend', () => {
   test('can be initalised with a valid OpenAPI document', async () => {
+    // @TODO: read complex document with as many features as possible here
     const opts = { document };
     expect(new OpenAPIBackend(opts)).toBeInstanceOf(OpenAPIBackend);
   });
@@ -14,5 +65,55 @@ describe('OpenAPIBackend', () => {
   test('throws an error when initalised with an invalid document', async () => {
     const opts: any = { document: { invalid: 'not openapi' } };
     expect(() => new OpenAPIBackend(opts)).toThrowError();
+  });
+
+  describe('.matchOperation', () => {
+    test('matches GET /pets', async () => {
+      const api = new OpenAPIBackend({ document });
+      const { operationId } = api.matchOperation('/pets', 'get');
+      expect(operationId).toEqual('getPets');
+    });
+
+    test('matches POST /pets', async () => {
+      const api = new OpenAPIBackend({ document });
+      const { operationId } = api.matchOperation('/pets', 'post');
+      expect(operationId).toEqual('createPet');
+    });
+
+    test('matches GET /pets/{id}', async () => {
+      const api = new OpenAPIBackend({ document });
+      const { operationId } = api.matchOperation('/pets/1', 'get');
+      expect(operationId).toEqual('getPetById');
+    });
+
+    test('matches PUT /pets/{id}', async () => {
+      const api = new OpenAPIBackend({ document });
+      const { operationId } = api.matchOperation('/pets/1', 'put');
+      expect(operationId).toEqual('replacePetById');
+    });
+
+    test('matches PATCH /pets/{id}', async () => {
+      const api = new OpenAPIBackend({ document });
+      const { operationId } = api.matchOperation('/pets/1', 'patch');
+      expect(operationId).toEqual('updatePetById');
+    });
+
+    test('matches DELETE /pets/{id}', async () => {
+      const api = new OpenAPIBackend({ document });
+      const { operationId } = api.matchOperation('/pets/1', 'delete');
+      expect(operationId).toEqual('deletePetById');
+    });
+
+    test('matches GET /pets/{id}/owner', async () => {
+      const api = new OpenAPIBackend({ document });
+      const { operationId } = api.matchOperation('/pets/1/owner', 'get');
+      expect(operationId).toEqual('getOwnerByPetId');
+    });
+
+    test('matches GET /pets/meta', async () => {
+      const api = new OpenAPIBackend({ document });
+      const { operationId } = api.matchOperation('/pets/meta', 'get');
+      expect(operationId).toEqual('getPetsMeta');
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import _ from 'lodash';
 import { OpenAPIV3 } from 'openapi-types';
 import { validate } from 'openapi-schema-validation';
 
 interface ConstructorOpts {
   document: OpenAPIV3.Document;
 }
+
+type HttpMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
 export default class OpenAPIBackend {
   public static validateDefinition(definition: OpenAPIV3.Document) {
@@ -19,5 +22,39 @@ export default class OpenAPIBackend {
 
   constructor(opts: ConstructorOpts) {
     this.document = OpenAPIBackend.validateDefinition(opts.document);
+  }
+
+  public matchOperation(matchPath: string, matchMethod: HttpMethod): OpenAPIV3.OperationObject {
+    const operations = _.chain(this.document.paths)
+      // flatten operations into an array with path + method
+      .entries()
+      .flatMap(([path, methods]) =>
+        _.map(_.entries(methods), ([method, operation]) => ({
+          path,
+          method,
+          ...(operation as OpenAPIV3.OperationObject),
+        })),
+      )
+      .value();
+
+    // first check for an exact match
+    const exactMatch = _.find(
+      operations,
+      ({ path, method }) => method === matchMethod && path === matchPath.replace(/\/+$/, ''),
+    );
+    if (exactMatch) {
+      return exactMatch;
+    }
+
+    // check for path templating
+    const templateMatch = _.find(operations, ({ path, method }) => {
+      if (method !== matchMethod) {
+        return false;
+      }
+      // convert openapi path template to a regex pattern. {id} becomes ([^/]+)
+      const pathPattern = `^${path.replace(/\{.*\}/g, '([^/]+)').replace(/\//g, '\\/')}\\/?$`;
+      return Boolean(matchPath.match(new RegExp(pathPattern, 'g')));
+    });
+    return templateMatch;
   }
 }
