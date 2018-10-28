21e950e4367614f9cf079cbb2923a03a482b4d91
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2018-10-28 08:57:00 +0200

    Add super simple express example

---
+++ examples/express/index.js
@@ -0,0 +1,57 @@
+const OpenAPIBackend = require('openapi-backend').default;
+const express = require('express');
+const app = express();
+
+// define api
+const api = new OpenAPIBackend({
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+    },
+    paths: {
+      '/pets': {
+        get: {
+          operationId: 'getPets',
+          responses: {
+            200: { description: 'ok' },
+          },
+        },
+      },
+      '/pets/{id}': {
+        get: {
+          operationId: 'getPetById',
+          parameters: [
+            {
+              name: 'id',
+              in: 'path',
+              required: true,
+              schema: {
+                type: 'integer',
+              },
+            },
+          ],
+          responses: {
+            200: { description: 'ok' },
+          },
+        },
+      },
+    },
+  },
+  handlers: {
+    getPets: async (req, res) => res.status(200).json({ ok: true }),
+    getPetById: async (req, res) => res.status(200).json({ ok: true }),
+    notFound: async (req, res) => res.status(200).json({ err: 'not found' }),
+    validationFail: async (err, req, res) => res.status(200).json({ err }),
+  },
+});
+
+api.init();
+
+// use as express middleware
+app.use((req, res) => api.handleRequest(req, req, res));
+
+// start server
+app.listen(9000, () => console.info('api listening at http://localhost:9000'));
+
