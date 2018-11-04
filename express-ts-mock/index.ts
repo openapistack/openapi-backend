4c24bf6ae6d384dcaffb1919d4134c011d8144cd
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2018-11-04 19:25:13 +0200

    Move defs in mock example to yaml file

--- express-ts-mock/index.ts
+++ express-ts-mock/index.ts
@@ -1,4 +1,5 @@
 import 'source-map-support/register';
+import path from 'path';
 import OpenAPIBackend from 'openapi-backend';
 import express from 'express';
 import morgan from 'morgan';
@@ -9,83 +10,7 @@ const app = express();
 
 // define api
 const api = new OpenAPIBackend({
-  definition: {
-    openapi: '3.0.1',
-    info: {
-      title: 'My API',
-      version: '1.0.0',
-    },
-    paths: {
-      '/pets': {
-        get: {
-          operationId: 'getPets',
-          responses: {
-            200: { $ref: '#/components/responses/ListPetsRes' },
-          },
-        },
-      },
-      '/pets/{id}': {
-        get: {
-          operationId: 'getPetById',
-          responses: {
-            200: { $ref: '#/components/responses/GetPetByIdRes' },
-          },
-        },
-        parameters: [
-          {
-            name: 'id',
-            in: 'path',
-            required: true,
-            schema: {
-              type: 'integer',
-            },
-          },
-        ],
-      },
-    },
-    components: {
-      responses: {
-        ListPetsRes: {
-          description: 'ok',
-          content: {
-            'application/json': {
-              schema: {
-                type: 'array',
-                items: {
-                  type: 'object',
-                  properties: {
-                    id: {
-                      type: 'integer',
-                      minimum: 1,
-                    },
-                    name: {
-                      type: 'string',
-                      example: 'Odie',
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-        GetPetByIdRes: {
-          description: 'ok',
-          content: {
-            'application/json': {
-              examples: {
-                garfield: {
-                  value: {
-                    id: 1,
-                    name: 'Garfield',
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+  definition: path.join(__dirname, '..', 'openapi.yml'),
   handlers: {
     validationFail: async (c, req: Request, res: Response) => res.status(400).json({ err: c.validation.errors }),
     notFound: async (c, req: Request, res: Response) => res.status(404).json({ err: 'not found' }),
