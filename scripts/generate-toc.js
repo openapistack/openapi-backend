9ed1da1a3277e983a74e8099ef6670aaa9f689ed
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2018-12-04 07:45:43 +0200

    Add missing scripts

---
+++ scripts/generate-toc.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const Toc = require('markdown-toc');
+
+function main() {
+  const filename = path.join(__dirname, '..', 'DOCS.md');
+  const docs = fs.readFileSync(filename, 'utf8');
+  const opts = {
+    bullets: '-',
+    slugify: (text) => text.toLowerCase()
+      .replace(/\s/g, '-')
+      .replace(/[^\w-]/g, ''),
+  };
+  const output = Toc.insert(docs, opts);
+  fs.writeFileSync(filename, output);
+  console.log('Done!');
+}
+
+if (require.main === module) {
+  main();
+}
+
