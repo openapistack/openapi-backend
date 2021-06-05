c34c97e53f94034dee2908f25f724b9e39cf4c0b
Viljami Kuosmanen <v.kuosmanen@epilot.cloud>
2021-06-05 14:16:37 +0200

    Update deps for examples

--- examples/express-ts-mock/index.ts
+++ examples/express-ts-mock/index.ts
@@ -1,10 +1,10 @@
 import 'source-map-support/register';
 import path from 'path';
-import OpenAPIBackend from 'openapi-backend';
+import OpenAPIBackend, { Request } from 'openapi-backend';
 import express from 'express';
 import morgan from 'morgan';
 
-import { Request, Response } from 'express';
+import { Request as ExpressReq, Response as ExpressRes } from 'express';
 
 const app = express();
 app.use(express.json());
@@ -13,9 +13,9 @@ app.use(express.json());
 const api = new OpenAPIBackend({
   definition: path.join(__dirname, '..', 'openapi.yml'),
   handlers: {
-    validationFail: async (c, req: Request, res: Response) => res.status(400).json({ err: c.validation.errors }),
-    notFound: async (c, req: Request, res: Response) => res.status(404).json({ err: 'not found' }),
-    notImplemented: async (c, req: Request, res: Response) => {
+    validationFail: async (c, req: ExpressReq, res: ExpressRes) => res.status(400).json({ err: c.validation.errors }),
+    notFound: async (c, req: ExpressReq, res: ExpressRes) => res.status(404).json({ err: 'not found' }),
+    notImplemented: async (c, req: ExpressReq, res: ExpressRes) => {
       const { status, mock } = c.api.mockResponseForOperation(c.operation.operationId);
       return res.status(status).json(mock);
     },
@@ -27,7 +27,7 @@ api.init();
 app.use(morgan('combined'));
 
 // use as express middleware
-app.use((req, res) => api.handleRequest(req, req, res));
+app.use((req, res) => api.handleRequest(req as Request, req, res));
 
 // start server
 app.listen(9000, () => console.info('api listening at http://localhost:9000'));
