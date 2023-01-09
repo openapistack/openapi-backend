fc184585d607099eee41ff021485d990ad5befb0
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2023-01-09 20:10:03 +0300

    feat: add aws cdk example (#453)
    * feat: add aws cdk example

---
+++ aws-cdk/README.md
@@ -0,0 +1,63 @@
+# OpenAPI Backend AWS CDK Example
+
+[![License](http://img.shields.io/:license-mit-blue.svg)](http://anttiviljami.mit-license.org)
+
+Example project using [openapi-backend](https://github.com/anttiviljami/openapi-backend) with [AWS CDK](https://aws.amazon.com/cdk/)
+
+## QuickStart
+
+### Requirements
+
+- NodeJS and NPM
+- AWS and AWS CDK
+- (optional) [AWS SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-reference.html#serverless-sam-cli) (>= v1.65) and [Docker](https://docs.docker.com/get-docker/) for local `npm start` and `npm test`
+
+#### AWS Authentication
+
+Make sure to set up your AWS authentication correctly. This example assume you are using AWS SSO profiles, make sure you are authenticated with the correct profile. E.g. `aws sso login --profile your-profile`
+
+#### CDK Bootstrap
+
+To use this example, you need to make sure CDK is bootstrapped for your AWS account and region.
+
+```bash
+AWS_PROFILE=your-profile npx cdk bootstrap aws://YOUR_ACCOUNT_ID/YOUR_DEFAULT_REGION
+```
+
+### Run and Test
+
+```bash
+AWS_PROFILE=your-profile npx cdk deploy
+```
+
+To try the endpoints, first copy the API GW URL that the `npx cdk deploy` command has outputted. The output will look like;
+
+```
+openapi-backend-example.OpenAPIBackendHttpApiEndpoint = https://ie88ixpq7g.execute-api.eu-west-1.amazonaws.com
+```
+
+Set an environment variable for making the further calls easier:
+```bash
+export CDK_OUTPUT_API_GW_URL=https://ie88ixpq7g.execute-api.eu-west-1.amazonaws.com
+```
+
+Try the endpoints:
+
+```bash
+curl -i "$CDK_OUTPUT_API_GW_URL/pets"
+curl -i "$CDK_OUTPUT_API_GW_URL/pets/1"
+```
+
+### Clean Up the Resources
+
+If you would like to remove the example from your AWS account, run:
+
+```bash
+AWS_PROFILE=your-profile npx cdk destroy
+```
+
+## ! IMPORTANT ! Caveats with the Example
+
+- [HttpApi CORS Settings](https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-cors.html) related problems maybe hard to troubleshoot. If you are calling endpoints with `OPTIONS` method manually, make sure you are providing all necessary request headers(E.g. `Origin`, `Access-Control-Request-Method`) properly. Otherwise, you will get `204 No Content` response without any proper CORS response headers.
+- You may use `$default` integration of `HttpApi` if you don't need to set up CORS for your API, or you would like to set up CORS on application side.
+- `source-map-support/register` takes a signification amount of time to convert error stacktraces to proper ones. You may want to optimize or disable `source-map-support` when deploying to production environment.
---
+++ aws-cdk/src/main.ts
@@ -0,0 +1,80 @@
+import { App, CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigwv2 from '@aws-cdk/aws-apigatewayv2-alpha';
+import { CorsHttpMethod, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { resolve } from 'path';
+import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+
+const MAXIMUM_HTTP_API_INTEGRATION_TIMEOUT = Duration.seconds(29);
+
+export class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    const entrypointLambda = new NodejsFunction(this, 'EntrypointLambda', {
+      entry: resolve(__dirname, './lambdas/api-entrypoint.lambda.ts'),
+      description: 'OpenAPI Backend Entrypoint Lambda',
+      // NodeJS LTS with AWS SDK v3
+      runtime: Runtime.NODEJS_18_X,
+      // Cost-effective Processor Architecture
+      architecture: Architecture.ARM_64,
+      // Maximum time a given endpoint Lambda invoke can take
+      timeout: MAXIMUM_HTTP_API_INTEGRATION_TIMEOUT,
+      // Lambda code bundling options
+      bundling: {
+        // Enable Source Map for better error logs
+        sourceMap: true,
+        // Hook into Commands for adding the OpenAPI Specification to Output
+        commandHooks: {
+          beforeBundling: () => [],
+          beforeInstall: () => [],
+          // Add the OpenAPI specification to the Lambda bundle
+          afterBundling: (inputDir: string, outputDir: string) => [
+            `cp "${inputDir}/openapi.yml" "${outputDir}/openapi.yml"`,
+          ],
+        },
+        // Add bundled AWS SDK V3 and CDK dependencies to the externals
+        externalModules: ['@aws-sdk/*', '@aws-cdk/*', 'aws-cdk', 'aws-cdk-lib'],
+      },
+    });
+
+    const httpApi = new apigwv2.HttpApi(this, 'HttpApi', {
+      description: 'OpenAPI Backend Http Api',
+      corsPreflight: {
+        allowHeaders: ['content-type'],
+        allowMethods: [CorsHttpMethod.ANY],
+        allowOrigins: ['*'],
+      },
+    });
+
+    // let's create a separate proxy integration instead of using `$default` integration of `HttpApi`
+    // this way; `OPTIONS` requests will be handled by `HttpApi` instead of invoking your Lambda
+    // You can opt in to use `defaultIntegration` and change `notFound` to return `204` for `OPTIONS` requests
+    httpApi.addRoutes({
+      path: '/{proxy+}',
+      // ALL methods expect OPTIONS / ANY should be handled by our Lambda
+      methods: Object.values(HttpMethod).filter(
+        (method) => method !== HttpMethod.OPTIONS && method !== HttpMethod.ANY
+      ),
+      integration: new HttpLambdaIntegration(
+        'OpenAPIBackendIntegration',
+        entrypointLambda
+      ),
+    });
+
+    /* tslint:disable-next-line no-unused-expression */
+    new CfnOutput(this, 'OpenAPIBackendHttpApiEndpoint', {
+      value: httpApi.apiEndpoint,
+      description: 'OpenAPI Backend Example HttpApi Endpoint',
+    });
+  }
+}
+
+const app = new App();
+
+/* tslint:disable-next-line no-unused-expression */
+new MyStack(app, 'openapi-backend-example');
+
+app.synth();
